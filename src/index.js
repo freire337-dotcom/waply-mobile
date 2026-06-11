9e8e7bd0dc56cf0b8a2f8cfc73e04d8a7e11ed2d
Fernando <freire337@gmail.com>
2026-06-11 16:17:49 +0200

    aumentar limite payload a 10mb

--- src/index.js
+++ src/index.js
@@ -29,7 +29,8 @@ app.set('io', io);
 
 // ── Middlewares ───────────────────────────────────────────────────────────────
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // ── Rutas API ─────────────────────────────────────────────────────────────────
 app.use('/api/auth',          authRoutes);
